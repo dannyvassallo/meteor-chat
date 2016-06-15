39ddfba65f0c51ec2c09a3daafa2624dc80e7fcd
dannyvassallo <danielvassallo87@gmail.com>
2016-06-15 12:57:21 -0400

    strip out boilerplate content and update readme

--- README.md
+++ README.md
@@ -21,3 +21,45 @@ meteor
 ```
 Visit: http://localhost:3000/
 ```
+
+### Step 2: Strip Out Boilerplate
+
+```javascript
+// main.js
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.home.onCreated(function homeOnCreated() {
+
+});
+
+Template.home.helpers({
+
+});
+
+Template.home.events({
+
+});
+
+```
+
+
+```html
+<!-- main.html -->
+
+<head>
+  <title>A Simple Meteor Chat App</title>
+</head>
+
+<body>
+  {{> home}}
+</body>
+
+<template name="home">
+
+</template>
+
+```
--- client/main.js
+++ client/main.js
@@ -3,20 +3,14 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.home.onCreated(function homeOnCreated() {
+
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.home.helpers({
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.home.events({
+
 });
